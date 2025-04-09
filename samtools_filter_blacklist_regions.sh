036f273140cf9d1ec775c8610970e084f6122274
Payal B <pbpayal42@gmail.com>
2025-04-09 13:44:59 -0400

    Update samtools_filter_blacklist_regions.sh

--- samtools_filter_blacklist_regions.sh
+++ samtools_filter_blacklist_regions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /data/NHGRImito/Chipseq_tatyana_2021/sam_files
+cd /data/sam_files
 
 module load samtools/1.13
 
@@ -9,7 +9,7 @@ do
   echo "My file name is $file"
   echo "Running ----SAMTOOLS FILTER BAM FILES----"
   echo "Running $file"
-  samtools view -b -L /data/NHGRImito/atacseq/ENCFF547MET.bed -U $file_filtered.bam  -o /data/NHGRImito/Chipseq_tatyana_2021/sam_files/extra_output/out_$file $file
+  samtools view -b -L /data/atacseq/ENCFF547MET.bed -U $file_filtered.bam  -o /data/sam_files/extra_output/out_$file $file
   echo "The  output is filtered_$file"
 
 done
