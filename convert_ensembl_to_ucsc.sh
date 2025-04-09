e3c559150e85436995f7128f13fa3989467c2e9d
Payal B <pbpayal42@gmail.com>
2025-04-09 13:46:08 -0400

    Update convert_ensembl_to_ucsc.sh

--- convert_ensembl_to_ucsc.sh
+++ convert_ensembl_to_ucsc.sh
@@ -1,4 +1,4 @@
-cd /data/NHGRImito/2021_Chip_Tatyana/sam_files/Acetyl/sorted_filtered_bams
+cd /data/sam_files/Acetyl/sorted_filtered_bams
 
 module load ucsc
 
@@ -18,7 +18,7 @@ module load ucsc
 for file in $(ls *ucsc.bdg)
 do
 echo "The filename is $file"
-bedGraphToBigWig $file /data/NHGRImito/Payal_scripts/mm10.chrom.sizes $file.bw
+bedGraphToBigWig $file /data/mm10.chrom.sizes $file.bw
 done
 
 exit
