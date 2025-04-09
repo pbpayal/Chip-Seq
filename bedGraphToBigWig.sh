bdc8a75bcbffc15c2b763a80c21cabd24e7fa309
Payal B <pbpayal42@gmail.com>
2025-04-09 13:49:41 -0400

    Update bedGraphToBigWig.sh

--- bedGraphToBigWig.sh
+++ bedGraphToBigWig.sh
@@ -1,11 +1,11 @@
 module load ucsc
-cd /data/NHGRImito/2021_Chip_Tatyana/sam_files/Acetyl/sorted_filtered_bams
+cd /data/sam_files/Acetyl/sorted_filtered_bams
 
 module load ucsc
 for file in $(ls *ucsc.bdg)
 do
 echo "The filename is $file"
-bedGraphToBigWig $file /data/NHGRImito/Payal_scripts/mm10.chrom.sizes $file.bw
+bedGraphToBigWig $file /data/mm10.chrom.sizes $file.bw
 done
 exit
 
